9a3464efa0bd2140aafa56b6c1f13df0a14f8812
sunhao <sunhao@cnezsoft.com>
2026-01-22 08:57:41 +0800

    * [story #80642] support for showing space app menu in left menu.

--- module/index/ui/index.html.php
+++ module/index/ui/index.html.php
@@ -138,6 +138,13 @@ div
     setID('menu'),
     div
     (
+        setID('spaceHeading'),
+        icon('space'),
+        span(setClass('text')),
+        span(setClass('label size-xs ring-0 bg-gray bg-opacity-50 text-gray-400 scale-75'), $lang->workspaceAbbr)
+    ),
+    div
+    (
         setID('menuNav'),
         ul(setClass('nav'), setID('menuMainNav')),
         ul
