45acc9f4c503c23b218da981ad563d8dbd433de2
sunhao <sunhao@cnezsoft.com>
2026-04-10 15:57:25 +0800

    * [bug #70222] fix module menu error in some pages.

--- module/custom/model.php
+++ module/custom/model.php
@@ -271,7 +271,16 @@ class customModel extends model
         }
         elseif($module)
         {
-            $menuOrder = ($module == 'main' && isset($lang->menuOrder)) ? $lang->menuOrder : (isset($lang->menu->{$module}['menuOrder']) ? $lang->menu->{$module}['menuOrder'] : array());
+            $menuOrder = array();
+            if($module == 'main' && isset($lang->menuOrder))
+            {
+                $menuOrder = $lang->menuOrder;
+            }
+            else if(isset($lang->menu->{$module}))
+            {
+                $moduleMenu = is_object($lang->menu->$module) ? (array)$lang->menu->$module : $lang->menu->$module;
+                if(is_array($moduleMenu) && isset($moduleMenu['menuOrder'])) $menuOrder = $moduleMenu['menuOrder'];
+            }
             if($menuOrder)
             {
                 ksort($menuOrder);
@@ -458,7 +467,11 @@ class customModel extends model
 
         $allMenu = new stdclass();
         if($module == 'main' and !empty($lang->menu)) $allMenu = $lang->menu;
-        if($module != 'main' and isset($lang->menu->$module) and isset($lang->menu->{$module}['subMenu'])) $allMenu = $lang->menu->{$module}['subMenu'];
+        if($module != 'main' and isset($lang->menu->$module))
+        {
+            $moduleMenu = is_object($lang->menu->$module) ? (array)$lang->menu->$module : $lang->menu->$module;
+            if(is_array($moduleMenu) && isset($moduleMenu['subMenu'])) $allMenu = $moduleMenu['subMenu'];
+        }
         if($module == 'product' and isset($allMenu->branch)) $allMenu->branch = str_replace('@branch@', $lang->custom->branch, $allMenu->branch);
 
         /* 获取自定义过的导航。 */
